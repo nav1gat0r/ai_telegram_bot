3972a215483f842e6fb3098ccbc929a9c020150b
nav1gat0r <42157990+nav1gat0r@users.noreply.github.com>
2023-02-18 01:41:05 +0300

    Search by photo
    Added option to search by photo in telegram

--- python main.py
+++ python main.py
@@ -4,54 +4,87 @@ from background import keep_alive
 
 import telebot
 import openai
-import time
+import requests
 
-bot = telebot.TeleBot("YOUR_BOT_TOKEN")
-openai.api_key = "OPENAI_API"
+# Authenticate with OpenAI
+openai.api_key = "OPOENAI_API"
 
-# Initialize the conversation state dictionary
-conversations = {}
+# Authenticate with Telegram 2x times
+bot = telebot.TeleBot("YOUR_TELEGRAM_TOKEN")
+os.environ["TELEGRAM_TOKEN"] = "YOUR_TELEGRAM_TOKEN"
 
-@bot.message_handler(content_types=["text"])
+# Define a function to handle text messages
+@bot.message_handler(func=lambda message: True, content_types=['text'])
 def handle_text(message):
-    chat_id = message.chat.id
-    text = message.text
-
-    # Check if the chat_id already has a conversation state saved
-    if chat_id in conversations:
-        # If it does, append the new message to the conversation context
-        context = conversations[chat_id]['context'] + ' ' + text
-        # Save the updated conversation context
-        conversations[chat_id]['context'] = context
-    else:
-        # If it doesn't, create a new conversation state and context
-        context = text
-        conversations[chat_id] = {'context': context}
-
     # Show typing indication
-    bot.send_chat_action(chat_id, 'typing')
+    bot.send_chat_action(message.chat.id, 'typing')
+    
+    # Generate a prompt based on user's message
+    prompt = f"Пожалуйста, напишите продолжение для следующего параграфа:\n{message.text}\n\nПродолжение:"
 
-    # Generate the response using the OpenAI API
-    response = openai.Completion.create(
-        engine="davinci", 
-        prompt=context, 
-        max_tokens=512, 
-        n=1, 
-        stop=None, 
-        temperature=0.5
-    ).choices[0].text
+    # Call the OpenAI API to generate text based on the prompt
+    try:
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            temperature=0.7,
+            max_tokens=1024,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+    except Exception as e:
+        bot.reply_to(message, "Извините, произошла ошибка при обработке вашего запроса.")
+        return
 
-    # Split the response into multiple messages if it is too long
-    max_message_length = 2048
-    num_messages = int(len(response) / max_message_length) + 1
-    for i in range(num_messages):
-        start = i * max_message_length
-        end = (i + 1) * max_message_length
-        message_text = response[start:end]
-        bot.send_message(chat_id, message_text)
+    # Extract the text from the OpenAI API response
+    continuation = response.choices[0].text
 
-    # Update the conversation context with the response
-    conversations[chat_id]['context'] = context + ' ' + response
+    # Split the continuation into multiple messages if it exceeds the Telegram message length limit
+    for i in range(0, len(continuation), 4096):
+        # Send the message with typing indication
+        bot.send_chat_action(message.chat.id, 'typing')
+        bot.reply_to(message, continuation[i:i+4096])
+
+# Define a function to handle photo messages
+@bot.message_handler(func=lambda message: True, content_types=['photo'])
+def handle_photo(message):
+    # Show typing indication
+    bot.send_chat_action(message.chat.id, 'typing')
+    
+    # Get the file ID of the photo
+    file_id = message.photo[-1].file_id
+
+    # Call the Telegram API to get the file path of the photo
+    file_path = bot.get_file(file_id).file_path
+
+    # Download the photo from Telegram's servers
+    photo_url = f"https://api.telegram.org/file/bot{os.environ['TELEGRAM_TOKEN']}/{file_path}"
+    photo_data = requests.get(photo_url).content
+
+    # Call the OpenAI API to generate a caption for the photo
+    try:
+        response = openai.Completion.create(
+            engine="davinci",
+            prompt=f"Пожалуйста, дайте подпись к этой фотографии:\n{photo_url}\n\nПодпись:",
+            temperature=0.7,
+            max_tokens=1024,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+    except Exception as e:
+        bot.reply_to(message, "Извините, произошла ошибка при обработке вашего запроса.")
+        return
+
+    # Extract the caption from the OpenAI API response
+    caption = response.choices[0].text
+
+    # Split the caption into multiple messages if it exceeds the Telegram message length limit
+    for i in range(0, len(caption), 4096):
+        # Send the message with typing indication
+        bot.send_chat_action(message.chat.id, 'typing')
+        bot.reply_to(message, caption[i:i+4096])
 
 # Start the flask server in a separate thread in background
 keep_alive()
